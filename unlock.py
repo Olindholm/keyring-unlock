7fec0899c88519db7bc6c36f287825e320d9d17d
Osvald Lindholm <osvald.lindholm@gmail.com>
2019-12-21 10:12:55 +0100

    Implement entering of password to unlock popup

--- unlock.py
+++ unlock.py
@@ -3,9 +3,17 @@
 # (formally known as Osvald Lindholm)
 #
 
-import time
+import time, os
 from threading import Thread
 from keyring import get_credential as get_keyring_credential
+
+# Type your password here
+#
+# Warning! Your password will now be stored in plain text. That
+# is any program can read this file and your system is thus very
+# insecure. Make sure you take other security measures to insure
+# the safety of your system.
+password = ""
 
 
 # Triggers the keyring popup if the keyring is not unlocked already.
@@ -20,10 +28,18 @@ def trigger_keyring_unlock_popup():
 popup = Thread(target = trigger_keyring_unlock_popup)
 popup.start()
 
+def is_locked(): return popup.is_alive()
+
 # Wait a moment for the system to react
 # Then check if the system is locked or not
-time.sleep(1)
-locked = popup.is_alive()
+time.sleep(0.1)
+if (is_locked()):
+    if (password == ""):
+        print("The system is locked and a unlock popup has been triggered!")
+    else:
+        os.system("xdotool type " + password)
+        os.system("xdotool key Return")
 
-if (locked):
-    print('The system is locked and a unlock popup has been triggered!')
+        time.sleep(0.1)
+        if (is_locked()):
+            print("Unlock unsuccessful, incorrect password!")
